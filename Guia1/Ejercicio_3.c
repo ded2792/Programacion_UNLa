081f0a020a1c4519d204a591ef9d9844796fce4b
ded2792 <ded27@live.com.ar>
2025-08-14 22:51:08 -0300

    Secuencia de numeros

---
+++ Guia1/Ejercicio_3.c
@@ -0,0 +1,16 @@
+/*
+Escriba un programa C que cuente de 1 a 10 y cuya salida tenga la forma:
+1 2 3 4 5 6 7 8 9 10
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+printf("*** Secuencia de numeros ***\n");
+
+int i;
+for (i = 1; i < 11; i++){
+    printf("%d ", i);
+}
+return 0;
+}
